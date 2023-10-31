d56d468dce25469f177276c7c35432c22dfbf1e0
gevorg <gevorg_email>
2023-10-31 07:47:52 +0300

    features: add conftest, base page class

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
-def main():
 
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+def main() -> None:
+    ...
+
 
 if __name__ == "__main__":
     main()
